92f255639e65f08a967fa99919d7b3306b24030a
menadammi30-dotcom <menadammi30@gmail.com>
2025-12-24 20:49:03 +0100

    Add song data for various categories

---
+++ data.js
@@ -0,0 +1,43 @@
+const songs = [
+  // 🎧 MODERNE
+  {
+    title: "Ay Izem",
+    artist: "Matoub Lounès",
+    category: "Moderne",
+    video: "https://www.youtube.com/embed/VIDEO_ID"
+  },
+  {
+    title: "A Yemna",
+    artist: "Takfarinas",
+    category: "Moderne",
+    video: "https://www.youtube.com/embed/VIDEO_ID"
+  },
+
+  // 📀 CLASSIQUE
+  {
+    title: "A Vava Inouva",
+    artist: "Idir",
+    category: "Classique",
+    video: "https://www.youtube.com/embed/4bXz6Y1cJ4s"
+  },
+  {
+    title: "Tawriqt Tacebhant",
+    artist: "Lounis Aït Menguellet",
+    category: "Classique",
+    video: "https://www.youtube.com/embed/VIDEO_ID"
+  },
+
+  // 🪘 FOLKLORE
+  {
+    title: "A Yemma",
+    artist: "Nouara",
+    category: "Folklore",
+    video: "https://www.youtube.com/embed/VIDEO_ID"
+  },
+  {
+    title: "Avrid n Tzemre",
+    artist: "Djamel Allam",
+    category: "Folklore",
+    video: "https://www.youtube.com/embed/VIDEO_ID"
+  }
+];
